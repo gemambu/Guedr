283cd90035f8643c4413374628f888fcf63543c0
Gema Martinez <gemambu@gmail.com>
2017-10-31 21:04:27 +0100

    First commit

--- app/src/main/java/com/gmb/guedr/MainActivity.kt
+++ app/src/main/java/com/gmb/guedr/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gmb.guedr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.v(TAG, "he pasado por oncreate")
+
+        if (savedInstanceState != null){
+            Log.v(TAG, "savedInstranceState no es null: ${savedInstanceState["clave"]}")
+        } else {
+            Log.v(TAG, "savedInstranceState es null")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.v(TAG, "he pasado por onSaveInstanceState")
+
+
+        outState?.putString("clave", "valor")
+
     }
 }
