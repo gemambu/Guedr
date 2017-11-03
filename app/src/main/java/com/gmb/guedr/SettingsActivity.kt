3af07d27b99c5eea62e47afd9dbbfd0f6c7171a9
Gema Martinez <gemambu@gmail.com>
2017-11-03 11:03:37 +0100

    sesion 3, parte 1

--- app/src/main/java/com/gmb/guedr/SettingsActivity.kt
+++ app/src/main/java/com/gmb/guedr/SettingsActivity.kt
@@ -1,7 +1,14 @@
 package com.gmb.guedr
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_settings)
+    }
 }
