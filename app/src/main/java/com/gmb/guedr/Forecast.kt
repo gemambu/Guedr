a804acebc1fbf372febfba5e48a4613604455bbc
Gema Martinez <gemambu@gmail.com>
2017-11-06 21:41:48 +0100

    first fragment and tests

--- app/src/main/java/com/gmb/guedr/Forecast.kt
+++ app/src/main/java/com/gmb/guedr/Forecast.kt
@@ -10,7 +10,13 @@ data class Forecast(val maxTemp: Float,
         FAHRENHEIT
     }
 
-    protected fun toFahrenheit(celsius: Float) = celsius * 1.8f + 31
+    protected fun toFahrenheit(celsius: Float) = celsius * 1.8f + 32
+
+    init {
+        if (humidity !in 0f..100f){
+            throw IllegalArgumentException("Humidity should be between 0 and 100")
+        }
+    }
 
     fun getMaxTemp(units: TempUnit) = when (units) {
         TempUnit.CELSIUS -> maxTemp
